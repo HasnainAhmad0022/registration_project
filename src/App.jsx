811f251697ed3cd0986afcede4999db11642716b
HasnainAhmad0022 <hasnainahmad0022@gmail.com>
2025-01-06 20:42:36 +0500

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage/HomePage'
 import DisablePerson from './pages/DisablePerson/DisablePerson'
 import StudentForm from './pages/Student/StudentForm'
 import Login from './pages/Login/Login'
+import MemberPage from './pages/MemberPage/MemberPage'
 
 const App = () => {
   return (
@@ -12,7 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home-page" element={<HomePage />} />
-          {/* <Route path="/register/member" element={<MemberPage />} /> */}
+          <Route path="/register/member" element={<MemberPage />} />
           <Route path="/register-student" element={<StudentForm />} />
           <Route path="/register-disabled" element={<DisablePerson />} />
         </Routes>
